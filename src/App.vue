dc08e9e0db768fa3077a4f3c2fe76ad1b3de5223
rus9384 <rus9384@yandex.ru>
2026-09-24 00:23:54 +0300

    setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <button @click="count = 1e6 * Math.random()">
+    <span id="test" class="numeric">{{count.toLocaleString("en-US", {maximumSignificantDigits: 6, useGrouping: false})}}</span>
+  </button>
+</template>
